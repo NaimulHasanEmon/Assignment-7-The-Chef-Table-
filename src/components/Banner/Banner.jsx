b88cc77be49a9753e372607a15eb3903908887cf
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-01-05 12:27:07 +0600

    Banner Added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,34 @@
-import Chef from '../../assets/Images/Chef Photo.jpg'
+import Chef from '../../assets/Images/Chef Photo.jpg';
 
 const Banner = () => {
     return (
-        <div>
-            <img src={Chef} style={{borderRadius: '20px'}} alt="Chef Background Image" title='Chef Background Image' />
+        <div
+            className="relative rounded-3xl h-[70vh] sm:h-[80vh] md:h-screen mx-auto w-full bg-cover bg-center flex items-center justify-center"
+            style={{ backgroundImage: `url(${Chef})` }}
+        >
+            {/* Dark Overlay */}
+            <div className="absolute inset-0 bg-black opacity-40 rounded-3xl"></div>
+
+            {/* Contents */}
+            <div className="relative text-center text-white px-6 sm:px-10 md:px-20 lg:px-32 xl:px-52">
+                <p className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl leading-tight">
+                    Discover an exceptional cooking class tailored for you!
+                </p>
+                <p className="text-xs sm:text-sm md:text-base lg:text-lg mt-4 sm:mt-6">
+                    Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database,
+                    and solve 500+ coding problems to become an exceptionally well world-class Programmer.
+                </p>
+
+                {/* Buttons */}
+                <div className="flex flex-col sm:flex-row gap-4 mt-6 sm:mt-8 justify-center">
+                    <button className="btn btn-success rounded-full text-sm sm:text-base md:text-lg font-semibold px-6 py-2 hover:bg-green-600">
+                        Explore Now
+                    </button>
+                    <button className="btn btn-ghost border-white rounded-full text-sm sm:text-base md:text-lg font-semibold px-6 py-2 hover:bg-white hover:text-black">
+                        Our Feedback
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { CgProfile } from "react-icons/cg";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
 const Navbar = () => {
     return (
@@ -6,36 +7,25 @@ const Navbar = () => {
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
                     <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Home</a></li>
-                        <li>
-                        <a>Recipes</a>
-                        <ul className="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <MdOutlineRestaurantMenu />
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li><a>Home</a></li>
+                            <li>
+                            <a>Recipes</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                            </li>
+                            <li><a>About</a></li>
+                            <li><a>Contact</a></li>
                         </ul>
-                        </li>
-                        <li><a>About</a></li>
-                        <li><a>Contact</a></li>
-                    </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">Recipe Calories</a>
+                    <a className="btn btn-ghost text-lg md:text-xl lg:text-xl">Recipe Calories</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
@@ -56,12 +46,12 @@ const Navbar = () => {
 
                 {/* Search Bar */}
                 <div className="navbar-end">
-                    <div className="flex gap-2">
+                    <div className="flex gap-1 md:gap-2 lg:gap-2">
                         <div className="form-control">
-                        <input type="text" placeholder="Search" className="text-sm input input-bordered h-8 w-24 rounded-3xl md:w-auto" />
+                        <input type="text" placeholder="Search" className="text-xs md:text-sm lg:text-sm input input-bordered h-7 w-[69px] md:h-8 md:w-auto lg:h-8 lg:w-24 rounded-3xl" />
                         </div>
                         <div>
-                        <CgProfile style={{ height: '35px', width: '35px', color:'rebeccapurple'}} />
+                        <CgProfile className="h-7 w-7 md:h-8 md:w-8 lg:h-8 lg:w-8 text-purple-700"/>
                         </div>
                     </div>
                 </div>
