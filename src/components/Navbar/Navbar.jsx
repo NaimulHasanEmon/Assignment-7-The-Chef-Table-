808d7985471a68dbaf868968dea89adcf636f4e2
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-01-06 19:03:40 +0600

    Recipe option sub-options updated and turned into hover feature.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,22 +4,22 @@ import { MdOutlineRestaurantMenu } from "react-icons/md";
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100 relative z-10">
                 <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <MdOutlineRestaurantMenu />
+                    <div className="dropdown dropdown-hover">
+                        <div role="button" className="btn btn-ghost lg:hidden">
+                            <MdOutlineRestaurantMenu />
                         </div>
                         <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[20] mt-3 w-52 p-2 shadow"
+                        >
                             <li><a>Home</a></li>
                             <li>
-                            <a>Recipes</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
+                                <a>Recipes</a>
+                                <ul className="p-2">
+                                    <li><a>Submenu 1</a></li>
+                                    <li><a>Submenu 2</a></li>
+                                </ul>
                             </li>
                             <li><a>About</a></li>
                             <li><a>Contact</a></li>
@@ -29,29 +29,34 @@ const Navbar = () => {
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                    <li><a>Home</a></li>
-                    <li>
-                        <details>
-                            <summary>Recipes</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
+                        <li><a>Home</a></li>
+                        <li className="group relative">
+                            <a>Recipes</a>
+                            <ul className="absolute left-0 hidden group-hover:block z-20 md:mt-8 lg:mt-8 bg-slate-300 p-2 rounded-box shadow-lg md:w-48 lg:w-48">
+                                <li><a>Grilled Chicken Salad</a></li>
+                                <li><a>Spaghetti Bolognese</a></li>
+                                <li><a>Vegetable Stir Fry</a></li>
+                                <li><a>Lemon Herb Fish</a></li>
+                                <li><a>Chocolate Cake</a></li>
+                                <li><a>Caesar Salad</a></li>
                             </ul>
-                        </details>
-                    </li>
-                    <li><a>About</a></li>
-                    <li><a>Contact</a></li>
+                        </li>
+                        <li><a>About</a></li>
+                        <li><a>Contact</a></li>
                     </ul>
                 </div>
-
                 {/* Search Bar */}
                 <div className="navbar-end">
                     <div className="flex gap-1 md:gap-2 lg:gap-2">
                         <div className="form-control">
-                        <input type="text" placeholder="Search" className="text-xs md:text-sm lg:text-sm input input-bordered h-7 w-[69px] md:h-8 md:w-auto lg:h-8 lg:w-24 rounded-3xl" />
+                            <input
+                                type="text"
+                                placeholder="Search"
+                                className="text-xs md:text-sm lg:text-sm input input-bordered h-7 w-[69px] md:h-8 md:w-auto lg:h-8 lg:w-24 rounded-3xl"
+                            />
                         </div>
                         <div>
-                        <CgProfile className="h-7 w-7 md:h-8 md:w-8 lg:h-8 lg:w-8 text-purple-700"/>
+                            <CgProfile className="h-7 w-7 md:h-8 md:w-8 lg:h-8 lg:w-8 text-purple-700" />
                         </div>
                     </div>
                 </div>
